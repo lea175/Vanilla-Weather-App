c4618b025f2762a856a7c8d17b2640be7a7771c0
lea175 <55576593+lea175@users.noreply.github.com>
2022-08-28 18:45:31 +0200

    Uupdated the time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,18 @@
+function formatDate(timestamp){
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if (hours<10){
+        hours = `0${hours}`
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10){
+       minutes =  `0${minutes}`
+    }
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thurday", "Friday", "Saturday"];
+    let day = days[date.getDay()];
+    return `${day} ${hours}:${minutes}`
+}
+
 function displayTemperature(response){
 console.log(response);
 let city = document.querySelector("#city");
@@ -5,24 +20,19 @@ let descripton = document.querySelector("#weather-condition");
 let temperature = document.querySelector("#temp")
 let humidity = document.querySelector("#humidity")
 let wind = document.querySelector("#wind");
+let date = document.querySelector("#date-current")
 city.innerHTML = response.data.name;
 descripton.innerHTML = response.data.weather[0].description;
 temperature.innerHTML = Math.round(response.data.main.temp);
 humidity.innerHTML = Math.round(response.data.main.humidity);
 wind.innerHTML = Math.round(response.data.wind.speed);
+date.innerHTML = formatDate(response.data.dt*1000)
 }
 
-function currentDate (date){
-    let now = new Date();
-    let hour = now.getHours();
-    console.log(hour);
-
-}
-
-currentDate();
 
 
-city = "Berlin"
+
+city = "new york"
 let apiKey = "8193ca299e026ed6459f4b81cdd9bf21"
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
 
